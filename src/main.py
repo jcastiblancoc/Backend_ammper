f5b0bf0c708bfefb996793e1bbf8cde14306e836
jcastiblancoc <castiblanco.jeisson@gmail.com>
2025-01-19 01:24:07 -0500

    mofify cors and .env database url

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ app = FastAPI(title="PWA Ammper Test")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["https://peppy-cendol-150567.netlify.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
